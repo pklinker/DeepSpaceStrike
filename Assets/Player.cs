d1b68a8684e78b786e65f47c2ae1d16b749eeb4c
pklinker <pklinker@gmail.com>
2020-10-18 14:38:35 -0400

    Player ship now yaws, pitches, and rolls based on user input.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,27 +2,71 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+ * Y then X for local rotation is easiest
+ * 
+ */
 public class Player : MonoBehaviour
 {
     public float maxXRange = 6f;
+    public float maxYRange = 4.6f;
+    public float minYRange = -4.3f;
 
-    // Speed in meters per second.
-    [Tooltip("in ms^-1")][SerializeField] float xSpeed = 12f;
+
+    // X-axis speed in meters per second.
+    [Tooltip("in ms^-1")][SerializeField] float xSpeed = 17f;
+
+    // Y-axis speed in meters per second.
+    [Tooltip("in ms^-1")] [SerializeField] float ySpeed = 17f;
+
+    [Tooltip("in ms^-1")] [SerializeField] float positionPitchFactor = -5f;
+    [Tooltip("in ms^-1")] [SerializeField] float controlPitchFactor = -18f;
+    [Tooltip("in ms^-1")] [SerializeField] float positionYawFactor = 4f;
+    [Tooltip("in ms^-1")] [SerializeField] float controlRollFactor = -20f;
+
+
+    float horizontalThrow;
+    float verticalThrow;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = Input.GetAxis("Horizontal");
+        transform.localPosition = new Vector3(getXPosition(), getYPosition(), transform.localPosition.z);
+        processRotation();
+    }
+    
+    private void processRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControl = verticalThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControl;
+
+        float yaw = transform.localPosition.x * positionYawFactor; 
+        float roll = controlRollFactor * horizontalThrow;
+
+        transform.localRotation = Quaternion.Euler(pitch,yaw,roll);
+    }
+    private float getXPosition()
+    {
+        horizontalThrow = Input.GetAxis("Horizontal");
         float xOffset = Time.deltaTime * horizontalThrow * xSpeed;
+        //        print("X offset = " + xOffset);
         float rawNewXPosition = xOffset + transform.localPosition.x;
         rawNewXPosition = Mathf.Clamp(rawNewXPosition, -maxXRange, maxXRange);
-//        print("X offset = " + xOffset);
-        transform.localPosition = new Vector3(rawNewXPosition, transform.localPosition.y, transform.localPosition.z);
-
+        return rawNewXPosition;
     }
+    private float getYPosition()
+    {
+        verticalThrow = Input.GetAxis("Vertical");
+        float yOffset = Time.deltaTime * verticalThrow * ySpeed;
+        float rawNewYPosition = yOffset + transform.localPosition.y;
+        rawNewYPosition = Mathf.Clamp(rawNewYPosition, minYRange, maxYRange);
+        return rawNewYPosition;
+    }
+
 }
