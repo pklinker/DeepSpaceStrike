17424cfced7e0dd3bf5f359cbd18482f6e51d02a
pklinker <pklinker@gmail.com>
2020-10-24 15:52:42 -0400

    Add enemy death and explosions.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Enemy : MonoBehaviour
+{
+
+    AudioSource audioSource;
+    bool isDead;
+    Collider boxCollider;
+
+    [Header("Effects")]
+    [SerializeField] AudioClip deathExplosionAC;
+    [SerializeField] GameObject deathExplosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        AddNonTriggerBoxCollider();
+        isDead = false;
+    }
+    void AddNonTriggerBoxCollider()
+    {
+        boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnParticleCollision(GameObject other)
+    {
+        isDead = true;
+        Instantiate(deathExplosion, transform.position, Quaternion.identity);
+        //        print("Particles collided with enemy " + gameObject.name);
+        Destroy(gameObject);
+
+    }
+}
